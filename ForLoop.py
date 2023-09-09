aa386f9a409947087e8634a81eac5887e1b7462c
Dipak Bist <mbdds54@gmail.com>
2023-09-09 13:00:17 +0545

    For Loop

---
+++ ForLoop.py
@@ -0,0 +1,14 @@
+for items in (1,2,3,4,5):
+    print(items)
+for items in [1,2,3,4,5]:
+    print(items)
+    #Nested Loop
+for items in {1,2,3,4,5}:
+    for x in ['a','b','c']:
+        print(items,x)
+#prin [1,a]
+#print[1,b]
+#print[1,c]
+#print[2,a]
+#print[2,b]
+#print[2,c] and so on......
