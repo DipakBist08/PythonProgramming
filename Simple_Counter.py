07664bca56a403b57de249019ae02c73fc4794fb
Dipak Bist <mbdds54@gmail.com>
2023-09-09 22:05:49 +0545

    Counter that will calculate total items sum

---
+++ Simple_Counter.py
@@ -0,0 +1,10 @@
+#Simple Counter
+#In the given List using loopiing iterable list make total sum
+
+my_list=[1,2,3,4,5,6,7,8,9,10]
+counter=0
+for item in my_list:
+    counter +=item
+    print(counter) #this print will iterate one by one.
+
+print(counter) #Display single output
