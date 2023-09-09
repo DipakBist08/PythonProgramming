bb3a4a53f219c5554b6bffaa0edaf699130ab601
Dipak Bist <mbdds54@gmail.com>
2023-09-09 22:14:10 +0545

    Simple counter that will iterate items form the list and calculate sum of total items with refactor commit

--- Simple_Counter.py
+++ Simple_Counter.py
@@ -1,10 +1,12 @@
-#Simple Counter
-#In the given List using loopiing iterable list make total sum
+# Simple Counter
+# In the given List using looping iterable list make total sum
 
-my_list=[1,2,3,4,5,6,7,8,9,10]
-counter=0
+my_list = [1,2,3,4,5,6,7,8,9,10]
+counter = 0  # Define a counter
 for item in my_list:
-    counter +=item
-    print(counter) #this print will iterate one by one.
+    counter += item
+    print(counter)  # this print will iterate one by one.
 
-print(counter) #Display single output
+print(counter)  # Display single output.It should be more efficient rather than above print statement
+
+
