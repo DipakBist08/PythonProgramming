b5d7977b1a155023cefe08d710065821d57ec1ac
Dipak Bist <mbdds54@gmail.com>
2023-09-10 21:55:31 +0545

    Working with while loop

---
+++ WhileLoops.py
@@ -0,0 +1,22 @@
+#it will print from 0 to 10.
+i=0
+while i<=10:
+    print(i)
+    i+=1
+
+#Will run infinite loop untill the system crash.
+# i =0
+# while i<1:
+#     print(i)
+
+# #if the user don't know how many time will be looping
+# while True:
+#     input('Say Something: ')
+
+#untill run the program response == bye
+
+while True:
+    response=input('Say Something:')
+    if response=='bye':
+        print(response)
+        break
