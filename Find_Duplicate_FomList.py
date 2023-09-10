1d049da6688a4ddde533c1fdb304e54ed2c41165
Dipak Bist <mbdds54@gmail.com>
2023-09-10 23:21:43 +0545

    Find out Duplicate values form list

--- Find_Duplicate_FomList.py
+++ Find_Duplicate_FomList.py
@@ -1,9 +1,24 @@
-#Exercise :Find the Duplicate values from the list and store those duplicate values in duplicates list
-duplicate_List=['a','b','a','c','d','c','e','b','e','f']
+# #Exercise :Find the Duplicate values from the list and store those duplicate values in duplicates list
+# duplicate_List=['a','b','a','c','d','c','e','b','e','f']
+#
+# duplicates=[]
+# for value in duplicate_List:
+#     if duplicate_List.count(value)>1:
+#         if value not in duplicates:
+#             duplicates.append(value)
+# print(duplicates)
 
-duplicates=[]
+
+
+#findout specific duplicate value how many times repeated.
+duplicate_List = ['a', 'b', 'a', 'c', 'd', 'c', 'e', 'b', 'e', 'f']
+count_dict = {}
+
 for value in duplicate_List:
-    if duplicate_List.count(value)>1:
-        if value not in duplicates:
-            duplicates.append(value)
-print(duplicates)
+    if value in count_dict:
+        count_dict[value] += 1
+    else:
+        count_dict[value] = 1
+
+for key, value in count_dict.items():
+    print(f'The value "{key}" appears as a duplicate {value} times.')
