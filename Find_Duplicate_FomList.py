0c1cdf0181232bf66a5b4283fd4f2af8a1ef9d9c
Dipak Bist <mbdds54@gmail.com>
2023-09-10 23:23:48 +0545

    Findout Duplicate Values From List

--- Find_Duplicate_FomList.py
+++ Find_Duplicate_FomList.py
@@ -1,12 +1,12 @@
-# #Exercise :Find the Duplicate values from the list and store those duplicate values in duplicates list
-# duplicate_List=['a','b','a','c','d','c','e','b','e','f']
-#
-# duplicates=[]
-# for value in duplicate_List:
-#     if duplicate_List.count(value)>1:
-#         if value not in duplicates:
-#             duplicates.append(value)
-# print(duplicates)
+#Exercise :Find the Duplicate values from the list and store those duplicate values in duplicates list
+duplicate_List=['a','b','a','c','d','c','e','b','e','f']
+
+duplicates=[]
+for value in duplicate_List:
+    if duplicate_List.count(value)>1:
+        if value not in duplicates:
+            duplicates.append(value)
+print(duplicates)
 
 
 
