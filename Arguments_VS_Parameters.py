f1f595330012ea16dbb49bde08469c5e152f3811
Dipak Bist <mbdds54@gmail.com>
2023-09-11 21:27:31 +0545

    Parameters VS Arguments

---
+++ Arguments_VS_Parameters.py
@@ -0,0 +1,11 @@
+# Parameteres are the variable and Arguments are the actual value we provide when we call a function.
+#We can cay Parameteres are like placeholders and to fill those placeholder we provide arguments.
+
+
+def My_Function(name,age): #name and age are parameters
+    print(f'Hello,{name},you are {age} years old.')
+
+    #Arguments Dipak and 27 are the arguments.
+My_Function('Dipak',27)
+My_Function('Ravi',22)
+My_Function('Sunil',20)
