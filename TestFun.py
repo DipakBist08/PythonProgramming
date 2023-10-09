0c5a02f8401acfda8695e8510e380e7ef018bb1c
Dipak Bist <mbdds54@gmail.com>
2023-10-10 00:08:13 +0545

    Create a Funtion with parameters and Arguments

---
+++ TestFun.py
@@ -0,0 +1,20 @@
+#write a function that has two parameters Name and Age and call it with arguments.
+def StudentInfo(name,age):
+    print(f'My name is {name} and I am {age} Years Old.')
+StudentInfo('Devid',23)
+#Taking Aruments from user.
+def StudentInfo1(Name,Age):
+    print(f'My Name is {Name} and I am {Age} Years Old.')
+
+name=input('Enter Your Name: ')
+age=int(input('Enter Your Age: '))
+
+StudentInfo1(name,age)
+
+
+
+
+
+
+
+
