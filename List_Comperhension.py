f9a8e4d7c2cdb3b10e8266b7e0bc4739f3b91c28
Dipak Bist <mbdds54@gmail.com>
2023-10-22 21:50:30 +0545

    List Comperhensions

--- List_Comperhension.py
+++ List_Comperhension.py
@@ -12,5 +12,13 @@ My_List2=[num for num in range(0,100)]
 print(My_List2)
 My_List3=[num*num for num in range(1,5)]
 print(My_List3)
-My_List3=[num**2 for num in range(1,5)]
-print(My_List3)
+My_List4=[num**2 for num in range(1,5)]
+print(My_List4)
+
+#To print Only Even Number form number 0 to 100
+Even_List=[num for num in range(0,100) if num%2==0]
+print(Even_List)
+#To print Odd Numbers only
+Odd_List=[nums for nums in range(0,100) if nums%2 !=0]
+print(Odd_List)
+
