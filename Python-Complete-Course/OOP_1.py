a7dedbc574564b8330c3039c58c4fd74d11ab7fa
DipakBist08 <mbdds54@gmail.com>
2025-03-15 17:50:35 +0545

    OOPs class and object

---
+++ Python-Complete-Course/OOP_1.py
@@ -0,0 +1,11 @@
+class Car:
+    brand ="Mercedes Benz"
+    color ="Blue"
+    Model = "2025"
+    seats = "2"
+
+obj = Car()
+print(obj.brand)
+print(obj.Model)
+print(obj.color)
+print(obj.seats)
