eca684bbb6f060f9de96298428f5a5e7a7564e4b
DipakBist08 <mbdds54@gmail.com>
2025-03-15 16:15:33 +0545

    First Program of OOPs Class and Object

---
+++ Python-Complete-Course/Class_and_Object.py
@@ -0,0 +1,14 @@
+class Student:
+    name = "Sunil Bista"
+    age =22
+    job = "Chef"
+
+s1 = Student()
+print(s1.name)
+print(s1.age)
+print(s1.job)
+
+s2 = Student()
+print(s2.name)
+print(s2.age)
+print(s2.job)
